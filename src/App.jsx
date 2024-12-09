ac8269f8f59e48f98f359432d8af5e0a195f6f7e
vani <vanibodasingu@gmail.com>
2024-12-09 11:47:01 +0530

    calculator

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
+<<<<<<< HEAD
 import React from 'react'
 import Counter from './components/Counter'
 import Todolist from './components/Todolist'
 import Meals from './components/Meals'
+=======
+import React from 'react';
+
+import Calcuu from './components/Calcuu';
+
+>>>>>>> 8310da5 (calculator)
 
 const App = () => {
   return (
     <div>
+<<<<<<< HEAD
      {/*<Counter/>*/}
      {/*<Todolist/>*/}
      <Meals/>
+=======
+    
+      <Calcuu/>
+>>>>>>> 8310da5 (calculator)
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,11 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
+<<<<<<< HEAD
  
+=======
+  
+>>>>>>> 8310da5 (calculator)
     <App />
  
 )
